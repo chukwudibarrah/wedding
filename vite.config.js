936a90c75c8ccd8e04729905ecbabda7e37d42cf
ceebrrh@gmail.com <118562380+chukwudibarrah@users.noreply.github.com>
2023-07-20 14:58:55 +0100

    updated yaml

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { splitVendorChunkPlugin } from 'vite';
-import dotenv from 'dotenv'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { splitVendorChunkPlugin } from "vite";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/wedding/",
   define: {
-    'process.env': {},
+    "process.env": {},
     __VALUE__: `"${process.env.VALUE}"`,
-    'process.env.VITE_SERVICE_ID': JSON.stringify(process.env.VITE_SERVICE_ID),
-'process.env.VITE_TEMPLATE_ID': JSON.stringify(process.env.VITE_TEMPLATE_ID),
-'process.env.VITE_PUBLIC_KEY': JSON.stringify(process.env.VITE_PUBLIC_KEY)
+    "process.env.VITE_SERVICE_ID": JSON.stringify(process.env.VITE_SERVICE_ID),
+    "process.env.VITE_TEMPLATE_ID": JSON.stringify(
+      process.env.VITE_TEMPLATE_ID
+    ),
+    "process.env.VITE_PUBLIC_KEY": JSON.stringify(process.env.VITE_PUBLIC_KEY),
   },
   plugins: [react(), splitVendorChunkPlugin()],
-})
+});
